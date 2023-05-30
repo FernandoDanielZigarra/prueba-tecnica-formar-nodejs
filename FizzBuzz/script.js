dfa640218f1629132aaac7773db30ae658ade06a
unknown <fernando.zigarra@gmail.com>
2023-05-30 19:06:44 -0300

    fix consoles.log in returns

--- FizzBuzz/script.js
+++ FizzBuzz/script.js
@@ -1,19 +1,35 @@
-let arr = [1];
+let input = process.argv[2];
 
-let regExpOnlyNumber = /[a-zA-Z!@#$%^&*(),.?":{}|<>]/;
-
-if (regExpOnlyNumber.test(process.argv[2]))
-  return console.log("El valor ingresado deve ser un número");
-
-for (let index = 1; index < process.argv[2]; index++) {
-  arr.push(arr.at(-1) + 1);
+function fizz_buzz(value) {
+  let regExpOnlyNumber = /[a-zA-Z!@#$%^&*(),.?":{}|<>]/;
+  if (regExpOnlyNumber.test(process.argv[2]))
+    return "El valor ingresado deve ser un número";
+  let arr = [];
+  let result = [];
+  for (let index = 1; index <= parseInt(value); index++) {
+    if (arr.length === 0) {
+      arr.push(1);
+      continue;
+    } else {
+      arr.push(arr.at(-1) + 1);
+      continue;
+    }
+  }
+  for (let i = 0; i <= arr.length - 1; i++) {
+    if (parseInt(arr[i]) % 3 === 0 && parseInt(arr[i]) % 5 === 0) {
+      result.push("FizzBuzz");
+      continue;
+    } else if (parseInt(arr[i]) % 3 === 0) {
+      result.push("Fizz");
+      continue;
+    } else if (parseInt(arr[i]) % 5 === 0) {
+      result.push("Buzz");
+      continue;
+    }
+    result.push(arr[i]);
+    continue;
+  }
+  return result;
 }
 
-function fizz_buzz() {
-  for (let i = 0; i < arr.length; i++)
-    console.log(
-      `${arr[i] % 3 ? "" : "Fizz"}${arr[i] % 5 ? "" : "Buzz"}` || arr[i]
-    );
-}
-
-fizz_buzz();
+console.log(fizz_buzz(input));
